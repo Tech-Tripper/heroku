555902cec02edc32d1e1a924522ca803d7244ecf
kelvoNgash <wamahenya82@gmail.com>
2019-01-22 10:52:50 -0500

    demo

---
+++ src/main/java/HerokuDemo/HerokuDemoApplication.java
@@ -0,0 +1,14 @@
+package HerokuDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/herokuDemo/controllers/MainControllers.java
@@ -0,0 +1,17 @@
+package herokuDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainControllers {
+	
+	
+	@GetMapping(value="/")
+	public  ModelAndView index() {
+		ModelAndView mv =new ModelAndView("index");
+	return mv;
+		    }
+
+}
